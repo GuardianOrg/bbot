72aca188512e498b06301e3b015def893b7fddc8
Carlos Polop <carlospolop@gmail.com>
2026-04-29 13:36:50 +0200

    Expose leaklookup_hash config options

--- bbot/modules/leaklookup_hash.py
+++ bbot/modules/leaklookup_hash.py
@@ -5,10 +5,11 @@ class leaklookup_hash(leaklookup):
     watched_events = ["HASHED_PASSWORD"]
     produced_events = ["PASSWORD"]
     flags = ["passive", "safe", "email-enum"]
+    options = leaklookup.options
+    options_desc = leaklookup.options_desc
     meta = {
         "description": "Query leak-lookup.com for plaintext passwords using hashes",
         "created_date": "2026-02-19",
         "author": "@carlospolop",
         "auth_required": True,
     }
-
